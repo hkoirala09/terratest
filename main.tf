03b48319c3a0f8287b2794fd2570fb75af96c1db
Hemanta Koirala <hkoirala@ncl.com>
2025-03-17 11:32:57 -0400

    validate

--- main.tf
+++ main.tf
@@ -1,74 +1,56 @@
-// Naming convention module for standardizing resource names
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
 module "naming-convention" {
   source  = "localterraform.com/azurepmr/naming-convention/azurerm"
   version = "1.0.10"
 }
 
-// CosmosDB MongoDB module
 module "cosmosdb-mongodb" {
-  source  = "localterraform.com/AzurePMR/cosmosdb-mongodb/azurerm"
+  source  = "localterraform.com/azurepmr/cosmosdb-mongodb/azurerm"
   version = "1.0.2"
 
-  # Required Variables
+  TFC_PROJECT_NAME    = var.TFC_PROJECT_NAME
+  TFC_WORKSPACE_NAME  = var.TFC_WORKSPACE_NAME
+  TFC_RUN_ID          = var.TFC_RUN_ID
   subscription_id     = var.subscription_id
   resource_group_name = var.resource_group_name
-  location           = var.location
+  location            = var.location
 
-  # Networking
-  private_endpoint_name          = "${module.naming-convention.short_resource}-pe-${module.naming-convention.short_location}-${module.naming-convention.short_env}"
-  virtual_network_resource_group_name = var.virtual_network_rg
-  virtual_network_name           = var.virtual_network_name
-  subnet_name                    = var.subnet_name
-  pe_resource_group_name         = var.pe_resource_group_name
+  account_name              = var.account_name
+  consistency_level         = var.consistency_level
+  offer_type                = var.offer_type
+  mongo_server_version      = var.mongo_server_version
+  capabilities              = var.capabilities
+  minimal_tls_version       = var.minimal_tls_version
+  backup_type               = var.backup_type
+  backup_tier               = var.backup_tier
+  total_throughput_limit    = var.total_throughput_limit
+  max_staleness_prefix      = var.max_staleness_prefix
+  max_interval_in_seconds   = var.max_interval_in_seconds
+  failover_priority         = var.failover_priority
+  uami_id                   = var.uami_id
+  encryption_key_name       = var.encryption_key_name
+  key_vault_id              = var.key_vault_id
 
-  # Identity
-  uami_id = azurerm_user_assigned_identity.adfumi.id
+  mongo_databases           = var.mongo_databases
+  mongo_collections         = var.mongo_collections
 
-  # Key Vault
-  encryption_key_name = var.encryption_key_name
-  key_vault_id       = data.azurerm_key_vault.key_vault.id
-
-  # CosmosDB Account
-  account_name           = "${module.naming-convention.short_resource}-cosmosdb-${module.naming-convention.short_location}-${module.naming-convention.short_env}"
-  consistency_level      = var.consistency_level
-  offer_type            = var.offer_type
-  mongo_server_version  = var.mongo_server_version
-  capabilities          = var.capabilities
-  minimal_tls_version   = var.minimal_tls_version
-  backup_type           = var.backup_type
-  backup_tier           = var.backup_tier
-  total_throughput_limit = var.total_throughput_limit
-  max_staleness_prefix   = var.max_staleness_prefix
-  max_interval_in_seconds = var.max_interval_in_seconds
-  failover_priority      = var.failover_priority
-
-  # CosmosDB Data Objects
-  mongo_databases   = var.mongo_databases
-  mongo_collections = var.mongo_collections
-
-  # Tags
-  tags = var.tags
-}
-
-#-----------------------------------------------------------#
-#                     Output Values                         #
-#-----------------------------------------------------------#
-output "env_short" {
-  value = module.naming-convention.short_env
-}
-
-output "location_short" {
-  value = module.naming-convention.short_location
-}
-
-output "resource_short" {
-  value = module.naming-convention.short_resource
-}
-
-output "cosmosdb_account_id" {
-  value = module.cosmosdb-mongodb.account_name
-}
-
-output "cosmosdb_endpoint" {
-  value = module.cosmosdb-mongodb.private_endpoint_name
+  private_endpoint_name               = var.private_endpoint_name
+  virtual_network_resource_group_name = var.virtual_network_resource_group_name
+  virtual_network_name                = var.virtual_network_name
+  subnet_name                         = var.subnet_name
+  pe_resource_group_name              = var.pe_resource_group_name
 }
--- variables.tf
+++ variables.tf
@@ -1,161 +1,170 @@
+terraform {
+  required_version = ">= 1.3"
+  experiments = [module_variable_optional_attrs]
+}
+
 variable "TFC_PROJECT_NAME" {
-  default = "Default Project"
+  type        = string
+  description = "Terraform Cloud Project Name"
 }
 
 variable "TFC_WORKSPACE_NAME" {
-  type = string
+  type        = string
+  description = "Terraform Cloud Workspace Name"
 }
 
 variable "TFC_RUN_ID" {
-  type = string
+  type        = string
+  description = "Terraform Cloud Run ID"
 }
 
 variable "subscription_id" {
-  type = string
-  description = "Subscription Id"
+  type        = string
+  description = "Azure Subscription ID"
 }
 
 variable "resource_group_name" {
-  type = string
-  description = "Resource group name"
+  type        = string
+  description = "Resource Group Name"
 }
 
 variable "location" {
-  type = string
-  description = "Azure Location"
-}
-
-variable "uami_id" {
-  type = string
-  description = "User Assigned Identity ID"
+  type        = string
+  description = "Azure Region"
+  default     = "eastus"
 }
 
 variable "account_name" {
-  type = string
-  description = "Name of CosmosDB account"
-}
-
-variable "offer_type" {
-  type = string
-  description = "Offer Type for Cosmos DB"
-  default = "Standard"
-}
-
-variable "total_throughput_limit" {
-  type = number
-  description = "CosmosDB account level throughput limit"
-  default = 1000
+  type        = string
+  description = "Cosmos DB Account Name"
 }
 
 variable "consistency_level" {
-  type = string
-  description = "Consistency Level of Cosmos DB account"
-  default = "Session"
+  type        = string
+  description = "Cosmos DB Consistency Level"
+  default     = "Session"
 }
 
-variable "backup_type" {
-  type = string
-  description = "Backup Type of Cosmos DB account"
-  default = "Continuous"
-}
-
-variable "backup_tier" {
-  type = string
-  description = "Backup Tier of Cosmos DB account"
-  default = "Continuous7days"
-}
-
-variable "minimal_tls_version" {
-  type = string
-  description = "TLS version Cosmos DB account"
-  default = "Tls12"
+variable "offer_type" {
+  type        = string
+  description = "Cosmos DB Offer Type"
+  default     = "Standard"
 }
 
 variable "mongo_server_version" {
-  type = string
-  description = "Mongo Server Version"
-  default = "4.0"
+  type        = string
+  description = "MongoDB Server Version"
+  default     = "4.0"
 }
 
 variable "capabilities" {
-  type = list(string)
-  description = "Capabilities of Mongo DB"
-  default = ["EnableMongo"]
+  type        = list(string)
+  description = "Capabilities for Cosmos DB"
+  default     = ["EnableMongo"]
 }
 
-variable "max_interval_in_seconds" {
-  type = number
-  description = "Max interval in seconds for Cosmos DB account consistency policy"
-  default = 300
+variable "minimal_tls_version" {
+  type        = string
+  description = "Minimum TLS Version"
+  default     = "Tls12"
+}
+
+variable "backup_type" {
+  type        = string
+  description = "Backup Type for Cosmos DB"
+  default     = "Continuous"
+}
+
+variable "backup_tier" {
+  type        = string
+  description = "Backup Tier for Cosmos DB"
+  default     = "Continuous7days"
+}
+
+variable "total_throughput_limit" {
+  type        = number
+  description = "Total Throughput Limit for Cosmos DB"
+  default     = 1000
 }
 
 variable "max_staleness_prefix" {
-  type = number
-  description = "Max stateless prefix for Cosmos DB account consistency policy"
-  default = 100000
+  type        = number
+  description = "Max Staleness Prefix"
+  default     = 100000
+}
+
+variable "max_interval_in_seconds" {
+  type        = number
+  description = "Max Interval in Seconds"
+  default     = 300
 }
 
 variable "failover_priority" {
-  type = number
-  description = "Failover priority for Cosmos DB geolocation settings"
-  default = 0
+  type        = number
+  description = "Failover Priority"
+  default     = 0
+}
+
+variable "uami_id" {
+  type        = string
+  description = "User Assigned Managed Identity ID"
+}
+
+variable "encryption_key_name" {
+  type        = string
+  description = "Encryption Key Name"
+}
+
+variable "key_vault_id" {
+  type        = string
+  description = "Key Vault ID"
 }
 
 variable "mongo_databases" {
-  description = "MongoDB databases configuration"
   type = map(object({
-    name  = string
-    throughput = optional(number)
-    autoscale_max_throughput = optional(number)
+    name                        = string
+    throughput                  = optional(number)
+    autoscale_max_throughput    = optional(number)
   }))
-  default = {}
+  description = "Mongo Databases Configuration"
+  default     = {}
 }
 
 variable "mongo_collections" {
-  description = "MongoDB collections configuration"
   type = map(object({
-    name  = string
-    database_name = string
-    default_ttl_seconds = number
-    shard_key = string
-    throughput = optional(number)
-    autoscale_max_throughput = optional(number)
-    keys = list(string)
+    name                        = string
+    database_name               = string
+    default_ttl_seconds         = number
+    shard_key                   = string
+    throughput                  = optional(number)
+    autoscale_max_throughput    = optional(number)
+    keys                        = list(string)
   }))
-  default = {}
+  description = "Mongo Collections Configuration"
+  default     = {}
 }
 
 variable "private_endpoint_name" {
-  type = string
-  description = "Name of Private Endpoint"
-}
-
-variable "pe_resource_group_name" {
-  type = string
-  description = "Private Endpoint resource group name"
+  type        = string
+  description = "Private Endpoint Name"
 }
 
 variable "virtual_network_resource_group_name" {
-  type = string
-  description = "Name of Resource Group of Virtual Network"
+  type        = string
+  description = "Virtual Network Resource Group Name"
 }
 
 variable "virtual_network_name" {
-  type = string
+  type        = string
   description = "Virtual Network Name"
 }
 
 variable "subnet_name" {
-  type = string
+  type        = string
   description = "Subnet Name"
 }
 
-variable "key_vault_id" {
-  type = string
-  description = "Key Vault ID"
-}
-
-variable "encryption_key_name" {
-  type = string
-  description = "Encryption Key name"
+variable "pe_resource_group_name" {
+  type        = string
+  description = "Private Endpoint Resource Group Name"
 }
